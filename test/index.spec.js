0d8b0640e1c73ac4d5ab9a8d861d19a934680241
Roman <romon2002@gmail.com>
2021-04-12 19:06:00 +0300

    Add more tests for isOk & isNOk

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,12 @@
 const result = require('../index');
 
-const funcs = ['isOk', 'isNOk', 'genOk', 'genNOk'];
+
+const badResults = () => ['', null, {}, [], undefined];
+const okResult = () => ({ status: 'ok', result: 'ok', data: [] });
+const nokResult = () => ({ status: 'nok', result: 'Some error!' });
 
 describe('All functions should be defined.', () => {
+  const funcs = ['isOk', 'isNOk', 'genOk', 'genNOk'];
   funcs.forEach((f) => {
     test(`Function ${f} defined`, () => {
       expect(result[f]).toBeDefined();
@@ -13,16 +17,32 @@ describe('All functions should be defined.', () => {
 
 describe('Test isOk function', () => {
   test('Should return false if no applicable result passed', () => {
-    ['', null, {}, [], undefined].forEach((res) => {
+    badResults().forEach((res) => {
       expect(result.isOk(res)).toBe(false);
     })
+  });
+
+  test('Should return true if OK result passed', () => {
+    expect(result.isOk(okResult())).toBe(true);
+  });
+
+  test('Should return false if not OK result passed', () => {
+    expect(result.isOk(nokResult())).toBe(false);
   });
 });
 
 describe('Test isNOk function', () => {
   test('Should return true if no applicable result passed', () => {
-    ['', null, {}, [], undefined].forEach((res) => {
+    badResults().forEach((res) => {
       expect(result.isNOk(res)).toBe(true);
     })
+  });
+
+  test('Should return true if not OK result passed', () => {
+    expect(result.isNOk(nokResult())).toBe(true);
+  });
+
+  test('Should return false if OK result passed', () => {
+    expect(result.isNOk(okResult())).toBe(false);
   });
 });
