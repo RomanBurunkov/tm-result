35a133437b819230b9b934020a7e9ea563ad31d7
Roman <romon2002@gmail.com>
2021-04-12 15:52:42 +0300

    Add isOk, isNOk, genOk, genNOk funcs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-exports.isOk = () => {
+const { isObject } = require('tm-is');
 
-};
+exports.isOk = (r) => (!isObject(r) ? false : r.status === 'ok' && r.result === 'ok');
 
-exports.isNOk = () => {
+exports.isNOk = (r) => (!isObject(r) ? true : r.status !== 'ok' || r.result !== 'ok');
 
-};
+exports.genOk = (data) => (typeof data === 'undefined'
+  ? { status: 'ok', result: 'ok' }
+  : { status: 'ok', result: 'ok', data }
+);
 
-exports.genOk = () => {
-
-};
-
-exports.genNOk = () => {
-
-};
+exports.genNOk = (r) => ({
+  status: 'nok',
+  result: isObject(r) && r instanceof Error ? r.message : r,
+});
